bd46f46f8cfa412ad428e6759efae7b6fba6c2a7
Andrei Albu <dinamo1999@icloud.com>
2024-12-31 15:28:02 +0100

    feat: add icon for services

--- AppConfig/AutomapperConfig.cs
+++ AppConfig/AutomapperConfig.cs
@@ -11,7 +11,8 @@ namespace TicDrive.AppConfig
             CreateMap<Service, FullServiceDto>()
                 .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
                 .ForMember(dest => dest.Title, opt => opt.MapFrom(src => src.Title))
-                .ForMember(dest => dest.Description, opt => opt.MapFrom(src => src.Description));
+                .ForMember(dest => dest.Description, opt => opt.MapFrom(src => src.Description))
+                .ForMember(dest => dest.Icon, opt => opt.MapFrom(src => src.Icon));
         }
     }
 }
--- Dto/ServiceDto/FullServiceDto.cs
+++ Dto/ServiceDto/FullServiceDto.cs
@@ -4,6 +4,7 @@
     {
         public int Id { get; set; }
         public required string Title { get; set; }
-        public string Description { get; set; } = "";
+        public string Description { get; set; } = string.Empty;
+        public string Icon { get; set; } = string.Empty;
     }
 }
