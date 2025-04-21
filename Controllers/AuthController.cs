bdf33874cdf52523989d5f619956c01ec24781f8
Andrei Albu <dinamo1999@icloud.com>
2025-04-21 16:08:38 +0200

    feat: add default profile image on user registration

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -97,6 +97,7 @@ namespace TicDrive.Controllers
                                 EmailConfirmed = false,
                                 UserType = payload.UserType,
                                 IsVerified = payload.IsVerified,
+                                ProfileImageUrl = "https://cdn1.iconfinder.com/data/icons/user-pictures/100/male3-512.png"
                             };
 
                             var result = await _userManager.CreateAsync(user, payload.Password);
