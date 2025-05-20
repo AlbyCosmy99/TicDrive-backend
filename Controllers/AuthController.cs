3fb80e4b8c58bdcef5156094344e77ab574ceedb
albycosmy99 <dinamo1999@icloud.com>
2025-05-20 19:01:04 +0200

    fix

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -66,7 +66,7 @@ namespace TicDrive.Controllers
 
             [Required]
             [MinLength(8, ErrorMessage = "Password must be at least 8 characters long.")]
-            [RegularExpression(@"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)[A-Za-z\d]{8,}$",
+            [RegularExpression(@"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)[A-Za-z\d@$!%*?&]{8,}$",
             ErrorMessage = "Password must contain at least one uppercase letter, one lowercase letter, and one number.")]
             public string Password { get; set; } = string.Empty;
 
