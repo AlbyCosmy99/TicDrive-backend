2ec622b6e02e7ae686cfc77485eaf362c869e180
albycosmy99 <dinamo1999@icloud.com>
2025-06-06 16:09:52 +0200

    feat: customerCar deletion is soft

---
+++ Migrations/20250606140356_AddSoftDeletableEntity.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TicDrive.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSoftDeletableEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Removed",
+                table: "CustomerCars",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Removed",
+                table: "CustomerCars");
+        }
+    }
+}
