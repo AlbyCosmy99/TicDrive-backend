79eec7870383ca71a7136de15b33a7540e06cb91
albycosmy99 <dinamo1999@icloud.com>
2025-06-08 12:41:34 +0200

    feat

---
+++ utils/datetime/DateTimeFormatter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace TicDrive.Utils.DateTime
+{
+    public static class DateTimeFormatter
+    {
+        public static string FormatLocalDateInItalian(System.DateTime localDate)
+        {
+            CultureInfo culture = new CultureInfo("it-IT");
+
+            string dayOfWeek = culture.DateTimeFormat.GetDayName(localDate.DayOfWeek);
+            string monthName = culture.DateTimeFormat.GetMonthName(localDate.Month);
+
+            dayOfWeek = char.ToUpper(dayOfWeek[0]) + dayOfWeek[1..];
+            monthName = char.ToUpper(monthName[0]) + monthName[1..];
+
+            return $"{dayOfWeek} {localDate.Day} {monthName} - {localDate:HH:mm}";
+        }
+    }
+}
